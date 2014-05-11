1b06fffa29717962ec6ef77b09df160078bfbe4b
Max <maxrogers90@gamil.com>
2014-05-10 20:04:15 -0400

    finished the interactive prep

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -13,13 +13,36 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.phonegap.plugins.videoplayer": "1.0",
-    "org.apache.cordova.device": "0.2.9"
+    "org.apache.cordova.device": "0.2.9",
+    "org.apache.cordova.vibration": "0.3.9-dev",
+    "org.apache.cordova.dialogs": "0.2.8-dev"
 }
 // BOTTOM OF METADATA
 });
