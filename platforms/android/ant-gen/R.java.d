573975b55f1eed82f1c55bec16e8abcbc5e5aa86
Max <maxrogers90@gamil.com>
2014-05-13 13:17:05 -0400

    fixed admin page and made debug apk availale

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -3,12 +3,16 @@
 /home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-xhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-ldpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/res/values/strings.xml \
+/home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-xxhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/res/xml/config.xml \
 /home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-hdpi/icon.png \
+/home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-xxxhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/res/drawable-mdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-xxhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-xxxhdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/res/drawable-mdpi/icon.png \
 /home/max/Dev/Cordova/Colon2/platforms/android/ant-build/AndroidManifest.xml \
